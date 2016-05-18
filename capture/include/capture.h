b125a5982df30bab15a79c4ac02de76d47f291bb
Turan Murat Guvenc <murat.guvenc@pro-line.com.tr>
2016-05-18 11:51:04 +0300

    created capture.h

---
+++ capture/include/capture.h
@@ -0,0 +1,19 @@
+/*
+ * capture.h
+ *
+ *  Created on: 18 May 2016
+ *      Author: Turan Murat Güvenç
+ */
+
+#ifndef INCLUDE_CAPTURE_H_
+#define INCLUDE_CAPTURE_H_
+
+#include <VideoFileCaptureFactory.h>
+#include <RTSPCaptureFactory.h>
+#include <WebcamCaptureFactory.h>
+#include <ICapture.h>
+#include <CaptureSettings.h>
+#include <Decoder.h>
+#include <VAFrameContainer.h>
+
+#endif /* INCLUDE_CAPTURE_H_ */
