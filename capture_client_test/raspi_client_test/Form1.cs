93a597e6f0990cd034c66511f07d064aa2041ce3
Turan Murat Güvenç <murat.guvenc@pro-line.com.tr>
2017-02-14 14:27:44 +0300

    capture api sample client added

---
+++ capture_client_test/raspi_client_test/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using Client;
+
+namespace raspi_client_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RaspiClient.initialize();
+        }
+
+        RaspiClient _client;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _client = new RaspiClient(pictureBox1, "tcp://192.168.200.25:5555", 640, 480, CodecType.H264);
+            _client.start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_client != null)
+            {
+                _client.stop();
+                _client.Dispose();
+            }
+        } 
+    }
+}
