0a11281a4e65d3ff56e087dfd4b94b3e23326fa5
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-03-01 17:28:42 +0300

    google test integration with parameterized tests

--- test/src/test_parser.cpp
+++ test/src/test_parser.cpp
@@ -1,31 +1,36 @@
 #include <parser.h>
 #include <gtest/gtest.h>
+#include <unordered_map>
 
-struct ArgumentParserTest : testing::Test
+typedef std::pair<std::string, std::string> Option;
+
+struct ArgumentParserTestAll : testing::TestWithParam<Option>
 {
 	ArgumentParser *parser;
 
-	ArgumentParserTest()
+	ArgumentParserTestAll()
 	{
 		parser = new ArgumentParser;
 	}
-	virtual ~ArgumentParserTest()
+	virtual ~ArgumentParserTestAll()
 	{
 		delete parser;
 	}
 };
 
-TEST_F(ArgumentParserTest, ThrowsWhenInvalidCodec)
+TEST_P(ArgumentParserTestAll, InvalidOptions)
 {
-	char *argv[] = { "capture-send", "-c", "asd" };
+	auto option = GetParam();
+	char *argv[] = { "capture-send", const_cast<char*>(option.first.c_str()), const_cast<char*>(option.second.c_str()) };
 	ASSERT_THROW(parser->parse(3, argv), std::invalid_argument);
 }
 
-TEST_F(ArgumentParserTest, ThrowsWhenInvalidFPS)
-{
-	char *argv[] = { "capture-send", "-f", "asd" };
-	ASSERT_THROW(parser->parse(3, argv), std::invalid_argument);
-}
+INSTANTIATE_TEST_CASE_P(Default, ArgumentParserTestAll, testing::Values(
+	std::make_pair("-c", "h265"),
+	std::make_pair("-f", "asd"),
+	std::make_pair("-u", ""),
+	std::make_pair("-a", "asdw")
+));
 
 int main(int argc, char* argv[])
 {
