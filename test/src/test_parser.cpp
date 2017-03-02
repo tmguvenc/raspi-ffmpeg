7ce8a95f53b49fea49a1a05f4c7dc901464392d1
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-03-02 09:59:24 +0300

    parser tests

--- test/src/test_parser.cpp
+++ test/src/test_parser.cpp
@@ -1,10 +1,24 @@
 #include <parser.h>
 #include <gtest/gtest.h>
 #include <unordered_map>
+#include <ostream>
 
-typedef std::pair<std::string, std::string> Option;
+struct ParserState
+{
+	std::string opt_name;
+	std::string opt_value;
+	bool throws;
 
-struct ArgumentParserTestAll : testing::TestWithParam<Option>
+	friend std::ostream& operator<<(std::ostream& os, const ParserState& obj)
+	{
+		return os
+			<< "opt_name: " << obj.opt_name
+			<< " opt_value: " << obj.opt_value
+			<< " throws: " << obj.throws;
+	}
+};
+
+struct ArgumentParserTestAll : testing::TestWithParam<ParserState>
 {
 	ArgumentParser *parser;
 
@@ -18,18 +32,49 @@ struct ArgumentParserTestAll : testing::TestWithParam<Option>
 	}
 };
 
-TEST_P(ArgumentParserTestAll, InvalidOptions)
+TEST_P(ArgumentParserTestAll, OptionValidation)
 {
-	auto option = GetParam();
-	char *argv[] = { "capture-send", const_cast<char*>(option.first.c_str()), const_cast<char*>(option.second.c_str()) };
-	ASSERT_THROW(parser->parse(3, argv), std::invalid_argument);
+	auto parser_state = GetParam();
+	char *argv[] = { "capture-send", const_cast<char*>(parser_state.opt_name.c_str()), const_cast<char*>(parser_state.opt_value.c_str()) };
+	if (parser_state.throws)
+		ASSERT_THROW(parser->parse(3, argv), std::invalid_argument);
+	else
+		ASSERT_NO_THROW(parser->parse(3, argv), std::invalid_argument);
 }
 
-INSTANTIATE_TEST_CASE_P(Default, ArgumentParserTestAll, testing::Values(
-	std::make_pair("-c", "h265"),
-	std::make_pair("-f", "asd"),
-	std::make_pair("-u", ""),
-	std::make_pair("-a", "asdw")
+INSTANTIATE_TEST_CASE_P(AllInvalid, ArgumentParserTestAll, testing::Values(
+	ParserState{ "-c", "h265", true },
+	ParserState{ "-f", "asd", true },
+	ParserState{ "-u", "", true },
+	ParserState{ "-a", "asdw", true },
+	ParserState{ "-a", "", true },
+	ParserState{ "-r", "1280*960", true },
+	ParserState{ "-c", "h264", false },
+	ParserState{ "-c", "mjpeg", false },
+	ParserState{ "-c", "raw", false },
+	ParserState{ "-c", "-raw", true },
+	ParserState{ "-c", "", true },
+	ParserState{ "-r", "1280x720", false },
+	ParserState{ "-r", "1920x-1080", true },
+	ParserState{ "-r", "asdxbnm", true },
+	ParserState{ "-r", "-1920xasd", true },
+	ParserState{ "-r", "asdx1080", true },
+	ParserState{ "-r", "", true },
+	ParserState{ "-f", "1234", true },
+	ParserState{ "-f", "", true },
+	ParserState{ "-f", "*0", true },
+	ParserState{ "-f", "0*", true },
+	ParserState{ "-f", "-1", true },
+	ParserState{ "-b", "-10", true },
+	ParserState{ "-b", "5", false },
+	ParserState{ "-b", "", true },
+	ParserState{ "-u", "/dev/video0", false },
+	ParserState{ "-u", "/dev/video1", false },
+	ParserState{ "-p", "asd", true },
+	ParserState{ "-p", "1234", false },
+	ParserState{ "-p", "1023", true },
+	ParserState{ "-p", "65537", true },
+	ParserState{ "-p", "-10", true }
 ));
 
 int main(int argc, char* argv[])
