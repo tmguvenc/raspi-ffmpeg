9188ed1a8f3cc1ddc25551ffddf7fa17d2d7d789
Turan Murat Guvenc <t.murat.guvenc@gmail.com>
2017-03-09 13:54:19 +0300

    frameQueue is not global now, try-catch added

--- rvms-server/src/main.cpp
+++ rvms-server/src/main.cpp
@@ -13,6 +13,7 @@
 #include <parser.h>
 #include <common_utils.h>
 
+
 int main(int argc, char* argv[]) {
 
 	auto logger = spdlog::stdout_color_mt("mainsender");
@@ -26,9 +27,6 @@ int main(int argc, char* argv[]) {
 		logger->error("{}", exception.what());
 		return -1;
 	}
-	tbb::concurrent_bounded_queue<FrameContainer*> frameQueue;
-
-	frameQueue.set_capacity(args.balance);
 
 	WebcamCaptureFactory captureFactory;
 
@@ -37,42 +35,44 @@ int main(int argc, char* argv[]) {
 	CaptureSettings settings(args.width, args.height, 3, args.fps, args.codec);
 
 	while (true) {
-		auto capture = captureFactory.create(args.url);
-		
-		capture->init(&settings);
-
-		capture->startAsync([&frameQueue](void* ptr) {
-			frameQueue.push(static_cast<Frame*>(ptr));
-		});
-
-		auto retry = 0;
-		while (!capture->started()) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			if (retry++ == 200){
-				logger->error("cannot open video source in {} sec.", 200 * 100 / 1000);
-				return -1;
-			}
-		}
-
 		try {
+			tbb::concurrent_bounded_queue<FrameContainer*> frameQueue;
+			frameQueue.set_capacity(args.balance);
+
+			auto capture = captureFactory.create(args.url);	
+			capture->init(&settings);
+
+			capture->startAsync([&frameQueue](void* ptr) {
+				frameQueue.push(static_cast<Frame*>(ptr));
+			});
+
+			auto retry = 0;
+			while (!capture->started()) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				if (retry++ == 200){
+					logger->error("cannot open video source in {} sec.", 200 * 100 / 1000);
+					return -1;
+				}
+			}
+
 			VideoFrameSender sender(args.port, args.width, args.height, args.codec);
 			sender.start([&frameQueue]() {
 				FrameContainer* frame;
 				frameQueue.pop(frame);
+				assert(frame != nullptr && "frame is null");
 				return frame;
 			});
+
+			clearQueue(&frameQueue);
+
+			logger->info<std::string>("Restarting capture-send");
+			assert(frameQueue.empty() && "frame queue is not empty!");
 		} catch (const std::exception& ex) {
 			logger->error("{}", ex.what());
 			return -1;
 		}
-
-		clearQueue(&frameQueue);
-
-		logger->info<std::string>("Restarting capture-send");
-		assert(frameQueue.empty() && "frame queue is not empty!");
 	}
 
-	clearQueue(&frameQueue);
 	spdlog::drop("mainsender");
 
 	return 0;
